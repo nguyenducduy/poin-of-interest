7ad1788057e0738a2bd881edde98320cb739d15f
Nguyen Duy <nguyenducduy.it@gmail.com>
2020-04-14 20:59:40 +0700

    update config.

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,7 +11,7 @@ instance.use(bodyParser.urlencoded({ extended: false }));
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, instance);
-    app.listen(9000, process.env.NODE_ENV === 'prod' ? '0.0.0.0' : '');
+    app.listen(9001, process.env.NODE_ENV === "prod" ? "0.0.0.0" : "");
 
     console.log("Application is listening on port 9000");
 }
